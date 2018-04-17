430de64e2f3e8774f8913fb15f003d728c4e7c24
lsamvura <lsamvura@psmi.co.zw>
2018-04-17 10:41:25 +0200

    Site list & site action

---
+++ src/main/java/zw/co/psmi/workShop/common/controller/SiteController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zw.co.psmi.workShop.common.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import zw.co.psmi.workShop.auth.entity.Login;
+import zw.co.psmi.workShop.common.entity.Site;
+import zw.co.psmi.workShop.common.service.SiteService;
+
+/**
+ *
+ * @author lionel
+ */
+
+@Controller
+public class SiteController {
+    @Autowired
+    private SiteService siteService;
+    
+    @RequestMapping(value = "/common/site", method = RequestMethod.GET)
+    public String site(@AuthenticationPrincipal Login userLogin, Model model) {
+        model.addAttribute("sites", this.siteService.findAll());
+        return "/common/site";
+    }
+
+    @RequestMapping(value = "/common/siteaction/{Id}", method = RequestMethod.GET)
+    public String siteAction(@AuthenticationPrincipal Login userLogin, @PathVariable("Id") Long Id, Model model) {
+        Site site = this.siteService.getByID(Id);
+        model.addAttribute("site", site);
+        return "/common/siteaction";
+    }
+
+    @RequestMapping(value = "/common/siteform", method = RequestMethod.POST)
+    public String siteform(@AuthenticationPrincipal Login userLogin, @ModelAttribute Site site, Model model, RedirectAttributes redirectAttributes) {
+        String msg = this.siteService.save(site);
+        redirectAttributes.addFlashAttribute("msg", "setMsg('" + msg + "')");
+        return "redirect:/common/siteform";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setAutoGrowCollectionLimit(500000);
+    }
+}
